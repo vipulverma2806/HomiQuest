0f37770e0d3013fa7b25303dccbd418887069678
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2025-06-28 22:07:29 +0530

    backend update

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
+dotenv.config()
+import cors from "cors"
+import userRouter from "./routes/user.route.js"
+import listingRouter from "./routes/listing.route.js"
+import bookingRouter from "./routes/booking.route.js"
+let port = process.env.PORT || 6000
+
+let app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use("/api/auth", authRouter )
+app.use("/api/user", userRouter )
+app.use("/api/listing",listingRouter )
+app.use("/api/booking",bookingRouter )
+
+
+app.listen(port,()=>{
+    connectDb()
+    console.log("server started")
+})
