b1d26806f95394f1903fe57bf8d8fb92416bdaa2
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2025-07-05 23:10:01 +0530

    added route

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { login, logOut, sighUp } from "../controllers/auth.controller.js"
+
+const authRouter = express.Router()
+
+authRouter.post("/signup",sighUp)
+authRouter.post("/login",login)
+authRouter.post("/logout",logOut)
+export default authRouter
