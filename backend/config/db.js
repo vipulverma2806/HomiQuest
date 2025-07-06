57515987e96ee0f6b65826e8e6d8a5e4987875cd
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2025-07-06 23:49:42 +0530

    commit

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("DB connected")
+    } catch (error) {
+        console.log("db error")
+    }
+}
+export default connectDb
